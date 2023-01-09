9d7c37fed53a7ad168b17b92392e5053ea2a2a97
mydmk7874 <121801956+mydmk7874@users.noreply.github.com>
2023-01-09 23:06:03 +0900

    Update my_place.py

--- my_place.py
+++ my_place.py
@@ -3,11 +3,33 @@ from db import db
 
 
 
-def my_place_list_get():
-    return jsonify({'place_list': ''})
+def my_place_list_get_api():
+    place_list = list(db.place.find({}, {'_id': False}))
+
+    return jsonify({'place_list': place_list})
 
 def my_place_delete():
+    num_receive = request.form['num_give']
+
+    db.place.delete_one({'num':num_recieve})
+
     return jsonify({'msg': '삭제 완료'})
 
 def my_place_post():
+    content_receive = request.form['content_give']
+    title_receive = request.form['title_give']
+    img_receive = request.form['img_give']
+
+    place_list = list(db.place.find({}, {'_id': False}))
+    count = len(place_list) + 1
+
+    doc = {
+        'num': count,
+        'title': title_receive,
+        'content': content_receive,
+        'img': img_receive
+    }
+
+    db.place.insert_one(doc)
+
     return jsonify({'msg': '등록 완료'})
