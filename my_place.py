6b3024a851ed96420d9cb4d42464b95d318e08a4
kikidy12 <kikidy12@naver.com>
2023-01-10 19:01:20 +0900

    my_place_post 에러 핸들링

--- my_place.py
+++ my_place.py
@@ -43,22 +43,29 @@ def my_place_delete():
 
 
 def my_place_post():
-    content = request.form['content']
-    title = request.form['title']
-    img = request.form['img']
-    category_num = request.form['category_num']
-    user_num = request.form['user_num']
+    try:
+        content = request.form['content']
+        title = request.form['name']
+        img = request.form['img']
+        category_num = request.form['category_num']
+        user_num = request.form['user_num']
 
-    # 장소목록을 num순으로 정렬
-    place_list = list(db.place.find({}, {'_id': False}).sort('num'))
+        # 장소목록을 num순으로 정렬
+        place_list = list(db.place.find({}, {'_id': False}).sort('num'))
 
-    # 마지막 번호를 호출한다
-    last_palce_num = place_list[-1]['num'] if len(place_list) > 0 else 1
+        # 마지막 번호를 호출한다
+        last_palce_num = place_list[-1]['num'] if len(place_list) > 0 else 1
 
-    category = db.category.find_one({'num': int(category_num)}, {'_id': False})
-    user = db.user.find_one({'num': int(user_num)}, {'_id': False})
+        category = db.category.find_one({'num': int(category_num)}, {'_id': False})
+        user = db.user.find_one({'num': int(user_num)}, {'_id': False})
 
-    db.place.insert_one(
-        {"num": last_palce_num + 1, "title": title, "content": content, 'img': img, 'category_num': category['num'],
-         'user_num': user['num']})
-    return jsonify({'msg': '등록 완료'})
+        db.place.insert_one(
+            {"num": last_palce_num + 1, "title": title, "content": content, 'img': img, 'category_num': category['num'],
+             'user_num': user['num']})
+        return jsonify({'msg': '등록 완료'})
+    except:
+        return {
+            'message': "Bad request!",
+            'status': 500,
+            'Error': 'Unexpected error.',
+        }, 500
