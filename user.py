aae5f45ebca025b49af3e6b29f0d5c34c15d95b6
mydmk7874 <mydmk7874@gmail.com>
2023-01-10 22:11:09 +0900

    회원가입 아이디 중복테스트 추가

--- user.py
+++ user.py
@@ -9,14 +9,14 @@ def sign_in():
     user = db.users.find_one({'id':id_receive})
 
     if user is None:
-        return jsonify({'msg': '해당하는 아이디가 없습니다'})
+        return jsonify({'msg': '아이디가 없거나 비밀번호가 틀립니다'})
     else :
 
         if user['password'] == password_receive :
-            return jsonify({'user': ''})
+            return jsonify({'user': user})
 
         else :
-            return jsonify({'msg': '비밀번호가 틀립니다'})
+            return jsonify({'msg': '아이디가 없거나 비밀번호가 틀립니다'})
 
 #회원가입
 def sign_up():
@@ -24,13 +24,19 @@ def sign_up():
     password_receive = request.form['password_give']
     nickname_receive = request.form['nickname_give']
 
-    user_list = list(db.users.find({}, {'_id': False}).sort('num'))
+    existing_id = db.users.find_one({'id': id_receive})
 
-    last_user_num = user_list[-1]['num'] if len(user_list) > 0 else 1
+    if existing_id is not None:
+        return jsonify({'msg': '이미 존재하는 ID입니다'})
+    else:
 
-    db.place.insert_one({"num": last_user_num + 1, "id": id_receive, "password": password_receive, "nickname": nickname_receive})
+        user_list = list(db.users.find({}, {'_id': False}).sort('num'))
 
-    return jsonify({'msg': '회원가입 완료'})
+        last_user_num = user_list[-1]['num'] if len(user_list) > 0 else 1
+
+        db.place.insert_one({"num": last_user_num + 1, "id": id_receive, "password": password_receive, "nickname": nickname_receive})
+
+        return jsonify({'msg': '회원가입 완료'})
 
 def user_get(token: str):
     return jsonify({'user': ''})
